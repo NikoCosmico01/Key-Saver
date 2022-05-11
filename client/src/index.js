b9329d684d4ee40abe007056ab7364f74befc34b
Nicolò Thei <NThei0130@GMail.Com>
2022-05-11 18:45:21 +0200

    Bootstrap on Navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from '../node_modules/react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
