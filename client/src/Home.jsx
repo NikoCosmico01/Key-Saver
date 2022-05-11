ee691921f04b1d4bc3c4c85e55b17af93d01cb58
Nicolò Thei <NThei0130@GMail.Com>
2022-05-11 18:42:18 +0200

    Navbar

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 function Home() {
     return(
         <div>
+            <Navbar />
             <h2>This will be the HomePage!!</h2>
         </div>
     );
--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,21 +1,22 @@
+import icon from './icon.jpg';
 
 function navbar() {
     return (
-        <nav class="navbar navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand">
-                    <img src={icon} alt="" width="30" height="24" class="d-inline-block align-text-top" />
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand">
+                    <img src={icon} alt="" width="30" height="24" className="d-inline-block align-text-top" />
                     &nbsp;Key Saver
                 </a>
-                <form class="d-flex">
+                <form className="d-flex">
                     <div>
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <div className="dropdown">
+                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Welcome 'Nome'
                             </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="#">Settings</a>
-                                <a class="dropdown-item" href="#">Logout</a>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a className="dropdown-item" href="#">Settings</a>
+                                <a className="dropdown-item" href="#">Logout</a>
                             </div>
                         </div>
                     </div>
@@ -24,3 +25,5 @@ function navbar() {
         </nav>
     );
 }
+
+export default navbar;
