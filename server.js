68172358cf694659bf0a3e6220b11dbd802dca7a
Nicolò Thei <NThei0130@GMail.Com>
2022-07-11 18:05:13 +0200

    Fixato Tutto

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 3001;
-
+//const PORT = 3001;
+const port = process.env.PORT || 5000;
 const {encrypt, decrypt} = require("./EncryptionHandler")
 
 app.use(cors())
