cc0364d38c0496c6000b267598fe2a8cac4abf06
Nicolò Thei <NThei0130@GMail.Com>
2022-07-11 16:35:37 +0200

    DB Setup

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+const mysql = require('mysql')
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: '',
+  database: 'PasswordManager'
+});
 
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
+app.post("/addpassword", (req, res) => { //Richiesta POST, dovrò fare una richiesta API
+  const {password, title} = req.body
+
+  db.query("INSERT INTO Passwords (Password, Title) VALUES (?, ?)", [password, title], (err, result) => { //Faccio la Insert e mi salvo eventuali errori in "err"
+    if (err) {
+      console.log(err) //Se ci sono errori li mostro in console
+    } else {
+      res.send("Completato con Successo") //Se NON ci sono errori mando una stringa di Success
+    }
+  })
+
+});
+
+// Mostra il Messaggio che il Server è Attivo su una specifica Porta
+app.listen(port, () => console.log(`In ascolto sulla Porta ${port}`));
+
+// crea un Percorso GET
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
