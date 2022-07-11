a978872e9ec82c37f3f019f5ece7ff4a2b547d99
Nicolò Thei <NThei0130@GMail.Com>
2022-07-11 16:45:09 +0200

    Update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,45 @@
 function Card(){
+    const [password, setPassword] = useState("");
+    const [title, setTitle] = useState("");
+
+    const addPassword = () => {
+        Axios.post('http://localhost:3000/addpassword', {
+            password: password,
+            title: title
+        });
+    };
+
     return(
-        <div className="card" style={{width: '18rem', textAlign: 'center' }}>
+    
+        /*<div className="card" style={{width: '18rem', textAlign: 'center' }}>
             <img className="card-img-top" src="..." alt="Card image cap"/>
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
+        </div>*/
+        
+        <div className="Card">
+            <div className="AddingPassword">
+
+                <input
+                type="text"
+                placeholder="Es.: Password1234"
+                onChange={(event) => {
+                    setPassword(event.target.value);
+                }}
+            />
+                <input
+                type="text"
+                placeholder="Es. Facebook"
+                onChange={(event) => {
+                    setTitle(event.target.value);
+                }}
+            />
+                <button onClick={addPassword}> Save</button>
+
+             </div>
         </div>
     )
 }
