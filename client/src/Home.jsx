08a4310b6d25ad305dc9ea63d6da8506045bdaf0
OpsCode00 <andrea.oppici1@studenti.unipr.it>
2022-07-14 12:25:39 +0200

    Card

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
-import { Avatar, Box, Container, Grid, IconButton } from '@mui/material';
+import { Avatar, Box, Card, Container, Grid, IconButton } from '@mui/material';
 import Navbar from './components/Navbar';
 import AccountCard from './components/AccountCard';
 import AddAccount from './components/AddAccount';
+import { Component } from 'react';
 
-function Home() {
-    return(
-        <Box>
-            <Navbar/>
-            <Container sx={{mt: 4, mb: 4}}>
-                <Grid container justifyContent="center" alignItems="center" spacing={3}>
-                    <AccountCard/>
-                    <AccountCard/>
-                    <AccountCard/>
-                    <AccountCard/>
-                    <AccountCard/>
-                    <AccountCard/>
-                    <AccountCard/>
-                    <AccountCard/>
-                    <AccountCard/>
-                </Grid>
-                <AddAccount/>     
-            </Container>
-            
-        </Box>
-    );
+class Home extends Component {
+    state = {
+        cards:[
+            {id: 0, title: "Amazon", firstChar: "A"},
+            {id: 1, title: "Twitter", firstChar: "T"},
+            {id: 2, title: "Google", firstChar: "G"},
+            {id: 3, title: "Linkedn", firstChar: "L"},
+            {id: 4, title: "Twitch", firstChar: "T"}
+        ]}
+    render() {
+        return(
+            <Box>
+                <Navbar/>
+                <Container sx={{mt: 4, mb: 4}}>
+                    <Grid container justifyContent="center" alignItems="center" spacing={3}>
+                        {this.state.cards.map(card => (
+                            <AccountCard
+                            key={card.id}
+                            title={card.title}
+                            firstchar={card.firstChar} />
+                        ))}
+                    </Grid>
+                    <AddAccount/>     
+                </Container>
+                
+            </Box>
+        );
+    }
 }
 
 export default Home;
--- client/src/components/AccountCard.jsx
+++ client/src/components/AccountCard.jsx
@@ -8,58 +8,39 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { Component } from 'react';
 import { Grid } from '@mui/material';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-export default function AccountCard() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  return (
-    <Grid item xs="auto">
-      <Card sx={{ maxWidth: 240, minWidth: 240 }}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
+export default class AccountCard extends Component{
+  render() {
+    return (
+      <Grid item xs="auto">
+        <Card sx={{ maxWidth: 240, minWidth: 240 }}>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                {this.props.firstchar}
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={this.props.title}
+          />
+          <CardContent>
+            
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <ContentCopyIcon />
             </IconButton>
-          }
-          title="Amazon"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to cook
-            together with your guests. Add 1 cup of frozen peas along with the mussels,
-            if you like.
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <ContentCopyIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </Grid>
-  );
+          </CardActions>
+        </Card>
+      </Grid>
+    );
+  }
 }
