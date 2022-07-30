5c46e00f1e0a1daea9136ab05ca74ac6e01ba698
Nicolò Thei <NThei0130@GMail.Com>
2022-07-30 10:18:49 +0200

    aa

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -7,73 +7,17 @@ import FormAddCard from './components/FormAddCard';
 import axios from 'axios';
 import { RestorePageRounded } from '@mui/icons-material';
 
-<<<<<<< HEAD
-class Home extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            sessionId: "",
-            accounts: []
-        }
-    }
-=======
+
 function Home(){
     const [accounts, setAccounts] = React.useState([]);
-<<<<<<< HEAD
->>>>>>> parent of 8404700ba (ops)
-=======
->>>>>>> parent of 8404700ba (ops)
-
-    componentDidMount(){
-        this.getAccountsList();
-        console.log(this.state.accounts)
-        /*fetch('http://localhost:5000/search')
-            .then(response => {return response.json()})
-            .then(data => {
-                data.forEach(account => {
-
-                })
-            })
-            .catch(err => console.log(err))*/
-    }
+}
 
     getAccountsList = () => {
         axios
             .get('http://localhost:5000/search')
-<<<<<<< HEAD
-<<<<<<< HEAD
             .then((response) => response.data)
             .then(response => this.setState({ accounts: response}))
     }
-
-    render() {
-        return(
-            <Box>
-                <Navbar/>
-                <Container sx={{mt: 4, mb: 4}}>
-                    <Grid container justifyContent="center" alignItems="center" spacing={3}>
-                        {this.state.accounts.map(account => (
-                            <AccountCard
-                            key={account.id}
-                            id={account.id}
-                            title={account.title}
-                            firstchar={account.title}
-                            username={account.username}
-                            password={account.password}
-                            />
-                        ))}
-                    </Grid>    
-                </Container>
-                <FormAddCard/> 
-                
-            </Box>
-        );
-    }
-=======
-=======
->>>>>>> parent of 8404700ba (ops)
-            .then(response => {setAccounts(response.data)})
-    });
 
     
     
@@ -97,7 +41,6 @@ function Home(){
             
         </Box>
     );
->>>>>>> parent of 8404700ba (ops)
-}
+
 
 export default Home;
